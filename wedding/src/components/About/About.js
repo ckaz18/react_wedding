adc2df36b1008aba316466cc49465a0e56ab910b
Christi Kazakov <ckazakov18@gmail.com>
2020-03-09 21:39:56 -0600

    Add fonts and images for about page - CK

--- wedding/src/components/About/About.js
+++ wedding/src/components/About/About.js
@@ -1,12 +1,26 @@
 import React from "react";
 import styles from "./About.module.css";
+import estes from "../../images/estes.png";
 
 const About = () => {
     return (
         <div>
             <section className={styles.main} id="main" data-section="about">
             About
-            <div className="float:none">Danielle Shriver & Billy Askew</div>
+            <h3>Danielle Shriver & Billy Askew</h3>
+            <div className={styles.aboutFont}>
+            <img src={estes} alt='Danielle and Billy' className={styles.rotateImage}/><br></br>
+                Join us for the Summer Solstice
+                <p>
+                    Saturday
+                </p>
+                <p>
+                    June 20th, 2020
+                </p>
+                <p>
+                    Sedalia, CO
+                </p>
+            </div>
             </section>
         </div>
     );
