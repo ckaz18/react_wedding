f97ebcf1d152fe3c251c3156d0ac0b0cda07c63a
Christi Kazakov <ckazakov18@gmail.com>
2020-03-09 21:06:21 -0600

    Update about and add css - CK

--- wedding/src/components/About/About.js
+++ wedding/src/components/About/About.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import styles from "./About.module.css";
 
 const About = () => {
     return (
         <div>
+            <section className={styles.main} id="main" data-section="about">
             About
+            <div className="float:none">Danielle Shriver & Billy Askew</div>
+            </section>
         </div>
     );
 };
